fd9a931e100fe22e99e90343ebd5153eb02e57c9
Davide <183734626+DavideP32@users.noreply.github.com>
2026-07-15 22:23:12 +0200

    feat(natural sorting) - implement natural sorting function to sort pdfs

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from pathlib import Path
 from pypdf import PdfWriter
+import re
+
+def natural_sort(element): 
+    file_name = element.name
+    # converte il testo a int se è solo numeri, altrimenti tutto in lowercase
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    # splitta i numeri dalle parole e le converte secondo il criterio di convert
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return alphanum_key(file_name)
+    
 
 p = Path('test-pdf')
 files = list(p.glob('*.pdf'))
-print(files)
+sorted_files = sorted(files, key=natural_sort)
+print(sorted_files)
+
 merger = PdfWriter()
 
-for pdf in files:
+for pdf in sorted_files:
     merger.append(pdf)
 
-merger.write("test-pdf/merged.pdf")
+merger.write(p / "merged.pdf")
+
+merger.close()
