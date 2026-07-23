a03081bc55dd12465287fb26056889870735f5ed
Davide <183734626+DavideP32@users.noreply.github.com>
2026-07-23 18:59:57 +0200

    feat(split components) - refactor: split logic into core and cli modules

---
+++ cli.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+from pypdf import PdfWriter #, PdfReader
+import sys
+import argparse
+from core import create_directory, sort_files, merge_pdf, MergeError
+
+
+def main():
+    output_folder_name = "merged"
+
+    # --------------------------------- ARGPARSE --------------------------------- #
+    parser = argparse.ArgumentParser(
+                        prog='pdf-merge-cli',
+                        description='Merge pdfs in a folder',
+                        epilog='ciao ciao')
+
+    parser.add_argument("-o", "--output", type=str, default="merged.pdf", help="name of the merged file")
+    parser.add_argument("folder", type=str, help="source folder for the pdfs to be merged")
+    parser.add_argument("--outline", help="optionally add outline to the merged pdf", action="store_true")
+    args = parser.parse_args()
+
+
+    output_file_name = args.output
+
+    if ("/" in output_file_name or "\\" in output_file_name):
+        parser.error("Output file name should not contain path separators.")
+
+
+    # ---------------------------- TAKE AND SORT FILES --------------------------- #
+
+    path_input_files = Path(args.folder)
+
+    if not path_input_files.is_dir():
+        print("The specified folder does not exist or is not a directory.", file=sys.stderr)
+        sys.exit(1)
+
+    list_of_files = list(path_input_files.glob('*.pdf'))
+
+    if not list_of_files:
+        print("No PDF file is present in the given folder.", file=sys.stderr)
+        sys.exit(1)
+
+
+    # ------------------------------ FUNCTION CALLS ------------------------------ #
+    output_path = create_directory(path_input_files=path_input_files, output_folder_name=output_folder_name)
+
+    sorted_files = sort_files(list_of_files=list_of_files)
+
+    try:
+        merge_pdf(sorted_files=sorted_files,
+                is_create_outline=args.outline,
+                output_path=output_path, 
+                output_file_name=output_file_name)
+    except MergeError as e:
+        print(f"Error reading PDF file: {e}", file=sys.stderr)
+        sys.exit(3)
+
+if __name__ == "__main__":
+    main()
---
+++ core.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+from pypdf import PdfWriter #, PdfReader
+from pypdf.errors import PdfStreamError
+import sys
+import re
+
+class MergeError(Exception): pass
+
+# --------------------------------- SORTING FUNCTIONS -------------------------------- #
+
+# sort files with natural sort method
+def natural_sort(element): 
+    file_name = element.name
+    return split_pdf_name(file_name)
+
+# split strings and numbers and convert each of them
+def split_pdf_name(name):
+    list_parts_name = re.split('([0-9]+)', name)
+    new_list = []
+    for part in list_parts_name:
+        new_list.append(convert_text(part))
+    return new_list
+        
+# Convert text to int if number or lowercase if text
+def convert_text(text):
+    return int(text) if text.isdigit() else text.lower()
+
+def create_directory(path_input_files, output_folder_name):
+    output_path = path_input_files / output_folder_name
+    output_path.mkdir(exist_ok=True)
+    return output_path
+
+def sort_files(list_of_files):
+    sorted_files = sorted(list_of_files, key=natural_sort)
+    return sorted_files
+
+# -------------------------------- MERGE FILES ------------------------------- #
+def merge_pdf(sorted_files, is_create_outline, output_path, output_file_name):
+    merger = PdfWriter()
+    for pdf in sorted_files:
+        try:
+            # Add the PDF to the merger, optionally adding an outline item
+            merger.append(pdf, outline_item=pdf.stem if is_create_outline else None)
+        except PdfStreamError as e:
+            merger.close()
+            raise MergeError(f"Error while appending PDF file: {pdf}") from e
+
+    # ---------------------------- CREATE FILE IN PATH --------------------------- #
+
+    merger.write(output_path / output_file_name)
+
+    merger.close()
